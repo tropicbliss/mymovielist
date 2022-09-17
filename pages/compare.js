9e54c0744e85740829bd9d9fbd2ee450164e354a
tropicbliss <tropicbliss@protonmail.com>
2022-09-18 00:30:49 +0800

    Initial work at greenfield comparison page

--- pages/compare.js
+++ pages/compare.js
@@ -1,50 +1,34 @@
+import { Fragment, useState } from "react";
+import { CheckIcon, MinusIcon } from "@heroicons/react/20/solid";
+import { classNames } from "../utilities";
+
 const compare = () => {
+  const [search1, setSearch1] = useState("");
+  const [search2, setSearch2] = useState("");
+
   return (
-    <div className="pt-16 flex justify-center space-x-3 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div className="overflow-hidden bg-gray-800 shadow rounded-lg">
-        <div className="px-4 py-5 sm:px-6">
-          <form onSubmit={(e) => redirectToMoviePage(e)}>
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-16">
+      <h1 className="text-center mb-16 text-4xl sm:text-5xl font-bold tracking-tight text-gray-900">
+        Compare movies
+      </h1>
+      <div className="grid grid-cols-2 divide-x divide-gray-900">
+        <div className="px-3">
+          <div className="relative mt-1 rounded-md shadow-sm">
             <input
-              id="search"
-              name="search"
-              className="block w-full text-xs rounded-md border border-transparent bg-gray-700 py-2 sm:pl-10 sm:pr-3 leading-5 text-gray-300 placeholder-gray-400 focus:border-white focus:bg-white focus:text-gray-900 focus:outline-none focus:ring-white sm:text-sm"
+              type="text"
+              className="block w-full rounded-md border-gray-300 pr-10 focus:border-cyber-purple focus:ring-cyber-purple text-sm sm:text-base"
               placeholder="Search for movies"
-              type="search"
             />
-          </form>
+          </div>
         </div>
-        <div className="border-t border-cyber-purple px-4 py-5 sm:p-0">
-          <dl className="sm:divide-y sm:divide-gray-200">
-            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-              <dt className="text-sm font-medium text-gray-300">Full name</dt>
-              <dd className="mt-1 text-sm text-gray-300 sm:col-span-2 sm:mt-0">
-                Margot Foster
-              </dd>
-            </div>
-          </dl>
-        </div>
-      </div>
-      <div className="overflow-hidden bg-gray-800 shadow rounded-lg">
-        <div className="px-4 py-5 sm:px-6">
-          <form onSubmit={(e) => redirectToMoviePage(e)}>
+        <div className="px-3">
+          <div className="relative mt-1 rounded-md shadow-sm">
             <input
-              id="search"
-              name="search"
-              className="block w-full text-xs rounded-md border border-transparent bg-gray-700 py-2 sm:pl-10 sm:pr-3 leading-5 text-gray-300 placeholder-gray-400 focus:border-white focus:bg-white focus:text-gray-900 focus:outline-none focus:ring-white sm:text-sm"
+              type="text"
+              className="block w-full rounded-md border-gray-300 pr-10 focus:border-cyber-purple focus:ring-cyber-purple text-sm sm:text-base"
               placeholder="Search for movies"
-              type="search"
             />
-          </form>
-        </div>
-        <div className="border-t border-cyber-purple px-4 py-5 sm:p-0">
-          <dl className="sm:divide-y sm:divide-gray-200">
-            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-              <dt className="text-sm font-medium text-gray-300">Full name</dt>
-              <dd className="mt-1 text-sm text-gray-300 sm:col-span-2 sm:mt-0">
-                Margot Foster
-              </dd>
-            </div>
-          </dl>
+          </div>
         </div>
       </div>
     </div>
