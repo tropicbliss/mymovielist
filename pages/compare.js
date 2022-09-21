f48f4c3f5da778465bca7f44ba68e397894b1764
tropicbliss <tropicbliss@protonmail.com>
2022-09-21 18:21:33 +0800

    Make movie compare page load only on successful fetch

--- pages/compare.js
+++ pages/compare.js
@@ -50,214 +50,186 @@ const compare = () => {
   };
 
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-16">
-      <Toast
-        show={show}
-        setShow={setShow}
-        isSuccess={false}
-        title="An error occured while searching"
-        description={errorMsg}
-      />
-      <h1 className="text-center mb-16 text-4xl sm:text-5xl font-bold tracking-tight text-gray-900">
-        Compare movies
-      </h1>
-      <div className="grid grid-cols-2 space-x-3">
-        <div className="relative mt-1 rounded-md shadow-sm">
-          <form onSubmit={(e) => onSubmit(e, 1)}>
-            <input
-              type="text"
-              className="block w-full rounded-md border-gray-300 pr-10 focus:border-cyber-purple focus:ring-cyber-purple text-sm sm:text-base"
-              placeholder="Spider-Man: No Way Home"
-              value={search1}
-              onChange={(e) => setSearch1(e.target.value)}
-            />
-          </form>
+    <>
+      {movieInfo1 && movieInfo2 && (
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-16">
+          <Toast
+            show={show}
+            setShow={setShow}
+            isSuccess={false}
+            title="An error occured while searching"
+            description={errorMsg}
+          />
+          <h1 className="text-center mb-16 text-4xl sm:text-5xl font-bold tracking-tight text-gray-900">
+            Compare movies
+          </h1>
+          {movieInfo1 && movieInfo2 && (
+            <div className="grid grid-cols-2 space-x-3">
+              <div className="relative mt-1 rounded-md shadow-sm">
+                <form onSubmit={(e) => onSubmit(e, 1)}>
+                  <input
+                    type="text"
+                    className="block w-full rounded-md border-gray-300 pr-10 focus:border-cyber-purple focus:ring-cyber-purple text-sm sm:text-base"
+                    placeholder="Spider-Man: No Way Home"
+                    value={search1}
+                    onChange={(e) => setSearch1(e.target.value)}
+                  />
+                </form>
+              </div>
+              <div className="relative mt-1 rounded-md shadow-sm">
+                <form onSubmit={(e) => onSubmit(e, 2)}>
+                  <input
+                    type="text"
+                    className="block w-full rounded-md border-gray-300 pr-10 focus:border-cyber-purple focus:ring-cyber-purple text-sm sm:text-base"
+                    placeholder="Top Gun: Maverick"
+                    value={search2}
+                    onChange={(e) => setSearch2(e.target.value)}
+                  />
+                </form>
+              </div>
+              <div className="p-3">
+                <h2 className="text-center text-2xl sm:text-3xl font-bold tracking-tight text-gray-900">
+                  {movieInfo1.info.Title}
+                </h2>
+              </div>
+              <div className="p-3">
+                <h2 className="text-center text-2xl sm:text-3xl font-bold tracking-tight text-gray-900">
+                  {movieInfo2.info.Title}
+                </h2>
+              </div>
+              {movieInfo1.poster && (
+                <div className="p-3 flex justify-center">
+                  <Image
+                    className="rounded"
+                    src={movieInfo1.poster}
+                    height="182px"
+                    width="146px"
+                    alt={`Movie poster of ${movieInfo1.info.Title}`}
+                  />
+                </div>
+              )}
+              {movieInfo2.poster && (
+                <div className="p-3 flex justify-center">
+                  <Image
+                    className="rounded"
+                    src={movieInfo2.poster}
+                    height="182px"
+                    width="146px"
+                    alt={`Movie poster of ${movieInfo2.info.Title}`}
+                  />
+                </div>
+              )}
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo1.info.Year ? movieInfo1.info.Year : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo2.info.Year ? movieInfo2.info.Year : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo1.info.Released ? movieInfo1.info.Released : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo2.info.Released ? movieInfo2.info.Released : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo1.info.Runtime ? movieInfo1.info.Runtime : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo2.info.Runtime ? movieInfo2.info.Runtime : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo1.info.Genre ? movieInfo1.info.Genre : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo2.info.Genre ? movieInfo2.info.Genre : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo1.info.Metascore
+                    ? "Metascore: " + movieInfo1.info.Metascore
+                    : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo2.info.Metascore
+                    ? "Metascore: " + movieInfo2.info.Metascore
+                    : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo1.info.imdbRating
+                    ? "IMDb Rating: " + movieInfo1.info.imdbRating
+                    : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo2.info.imdbRating
+                    ? "IMDb Rating: " + movieInfo2.info.imdbRating
+                    : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo1.info.tomatoMeter
+                    ? "Rotten Tomatoes: " + movieInfo1.info.tomatoMeter
+                    : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo2.info.tomatoMeter
+                    ? "Rotten Tomatoes: " + movieInfo2.info.tomatoMeter
+                    : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo1.info.BoxOffice ? movieInfo1.info.BoxOffice : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo2.info.BoxOffice ? movieInfo2.info.BoxOffice : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo1.info.Production
+                    ? movieInfo1.info.Production
+                    : "-"}
+                </p>
+              </div>
+              <div className="p-3">
+                <p className="text-center text-base sm:text-xl font-bold text-gray-900">
+                  {movieInfo2.info.Production
+                    ? movieInfo2.info.Production
+                    : "-"}
+                </p>
+              </div>
+            </div>
+          )}
         </div>
-        <div className="relative mt-1 rounded-md shadow-sm">
-          <form onSubmit={(e) => onSubmit(e, 2)}>
-            <input
-              type="text"
-              className="block w-full rounded-md border-gray-300 pr-10 focus:border-cyber-purple focus:ring-cyber-purple text-sm sm:text-base"
-              placeholder="Top Gun: Maverick"
-              value={search2}
-              onChange={(e) => setSearch2(e.target.value)}
-            />
-          </form>
-        </div>
-        {movieInfo1 && (
-          <div className="p-3">
-            <h2 className="text-center text-2xl sm:text-3xl font-bold tracking-tight text-gray-900">
-              {movieInfo1.info.Title}
-            </h2>
-          </div>
-        )}
-        {movieInfo2 && (
-          <div className="p-3">
-            <h2 className="text-center text-2xl sm:text-3xl font-bold tracking-tight text-gray-900">
-              {movieInfo2.info.Title}
-            </h2>
-          </div>
-        )}
-        {movieInfo1 && movieInfo1.poster && (
-          <div className="p-3 flex justify-center">
-            <Image
-              className="rounded"
-              src={movieInfo1.poster}
-              height="182px"
-              width="146px"
-            />
-          </div>
-        )}
-        {movieInfo2 && movieInfo2.poster && (
-          <div className="p-3 flex justify-center">
-            <Image
-              className="rounded"
-              src={movieInfo2.poster}
-              height="182px"
-              width="146px"
-            />
-          </div>
-        )}
-        {movieInfo1 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo1.info.Year ? movieInfo1.info.Year : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo2 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo2.info.Year ? movieInfo2.info.Year : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo1 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo1.info.Released ? movieInfo1.info.Released : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo2 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo2.info.Released ? movieInfo2.info.Released : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo1 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo1.info.Runtime ? movieInfo1.info.Runtime : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo2 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo2.info.Runtime ? movieInfo2.info.Runtime : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo1 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo1.info.Genre ? movieInfo1.info.Genre : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo2 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo2.info.Genre ? movieInfo2.info.Genre : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo1 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo1.info.Metascore
-                ? "Metascore: " + movieInfo1.info.Metascore
-                : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo2 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo2.info.Metascore
-                ? "Metascore: " + movieInfo2.info.Metascore
-                : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo1 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo1.info.imdbRating
-                ? "IMDb Rating: " + movieInfo1.info.imdbRating
-                : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo2 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo2.info.imdbRating
-                ? "IMDb Rating: " + movieInfo2.info.imdbRating
-                : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo1 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo1.info.tomatoMeter
-                ? "Rotten Tomatoes: " + movieInfo1.info.tomatoMeter
-                : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo2 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo2.info.tomatoMeter
-                ? "Rotten Tomatoes: " + movieInfo2.info.tomatoMeter
-                : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo1 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo1.info.BoxOffice ? movieInfo1.info.BoxOffice : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo2 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo2.info.BoxOffice ? movieInfo2.info.BoxOffice : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo1 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo1.info.Production ? movieInfo1.info.Production : "-"}
-            </p>
-          </div>
-        )}
-        {movieInfo2 && (
-          <div className="p-3">
-            <p className="text-center text-base sm:text-xl font-bold text-gray-900">
-              {movieInfo2.info.Production ? movieInfo2.info.Production : "-"}
-            </p>
-          </div>
-        )}
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
