f11e9175fbece6a97ea3e32e1eaf6ba46aae324d
tropicbliss <tropicbliss@protonmail.com>
2022-10-05 23:05:30 +0800

    Chat trim whitespace

--- pages/chat.js
+++ pages/chat.js
@@ -77,7 +77,7 @@ function ChatRoom() {
       <form className="flex space-x-1 sm:space-x-3" onSubmit={sendMessage}>
         <input
           value={formValue}
-          onChange={(e) => setFormValue(e.target.value)}
+          onChange={(e) => setFormValue(e.target.value.trim())}
           placeholder="Say something nice"
           className="w-full rounded-md border border-gray-300 px-5 py-3 placeholder-gray-400 shadow-sm focus:border-cyber-purple focus:ring-1 focus:ring-cyber-purple"
         />
