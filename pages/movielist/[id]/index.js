2c7f1d1014e6d8ba7e1b293e29446123bf01bdfd
tropicbliss <tropicbliss@protonmail.com>
2022-10-08 14:27:32 +0800

    Complete movie list page

--- pages/movielist/[id]/index.js
+++ pages/movielist/[id]/index.js
@@ -1,5 +1,31 @@
-const index = () => {
-  return <div>index</div>;
+import { doc, getDoc } from "firebase/firestore";
+import MovieList from "../../../components/MovieList";
+import { database } from "../../../firebaseConfig";
+
+const index = ({ id }) => {
+  return <MovieList uid={id} showName={true} />;
+};
+
+export const getServerSideProps = async (context) => {
+  let id = context.params.id;
+  try {
+    const userRef = doc(database, "completedList", id);
+    const userData = await getDoc(userRef);
+    if (!userData.exists()) {
+      return {
+        notFound: true,
+      };
+    }
+    return {
+      props: {
+        id,
+      },
+    };
+  } catch (e) {
+    return {
+      notFound: true,
+    };
+  }
 };
 
 export default index;
