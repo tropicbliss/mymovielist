14f3258a3f59f5e1456d446c5ac812fc2d0b03d7
tropicbliss <tropicbliss@protonmail.com>
2022-10-13 22:04:21 +0800

    E

--- components/Nav.js
+++ components/Nav.js
@@ -13,6 +13,7 @@ import { classNames } from "../utilities";
 import { LinearProgress } from "@mui/material";
 import { GlobalContext } from "../context/GlobalState";
 import Avatar from "./Avatar";
+import { H } from "highlight.run";
 
 async function signInWithGoogle() {
   const provider = new GoogleAuthProvider();
@@ -78,6 +79,14 @@ const Nav = () => {
     }
   };
   const [user] = useAuthState(auth);
+  if (auth) {
+    H.identify(user.email, {
+      id: user.uid,
+      name: user.displayName,
+      phone: user.phoneNumber,
+      photoURL: user.photoURL,
+    });
+  }
   useEffect(() => {
     Router.events.on("routeChangeStart", () => {
       setLoad(true);
--- pages/index.js
+++ pages/index.js
@@ -24,18 +24,6 @@ function Hero() {
         </p>
       </div>
     </main>
-    // <main className="mx-auto mt-16 max-w-7xl px-4 sm:mt-24">
-    //   <div className="text-center">
-    //     <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-    //       <span className="block xl:inline">
-    //         Welcome to the premier movie watcher&apos;s paradise
-    //       </span>
-    //     </h1>
-    //     <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
-    //       Get the latest entertainment news and movie info straight from us!
-    //     </p>
-    //   </div>
-    // </main>
   );
 }
 
